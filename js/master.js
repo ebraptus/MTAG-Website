c5343beae7e4173d426a0f2786ac6421a9713173
John de Shano <themaxtwo2@yahoo.com>
2017-12-01 08:39:24 -0500

    Update to add class 1's games

--- js/master.js
+++ js/master.js
@@ -58,7 +58,10 @@ function viewGame(iIndex){
 
 
 // Start of adding games
-AddGame("Game One", "Desc and shiz", "game1");
+AddGame("Dark Souls", "A darksouls based game where you are on a quest to save the the fallen lords", "darksouls");
+AddGame("Battle The Principle", "You get your phone taken from by the principle and you have to get it back", "principle");
+AddGame("Tom the Cat", "Tom the cat loves his peacful life at his house. but Jerry Nibbles and Spike invaded his home, help Tom get rid of these foes who have entered his home, fight enemies buy items from Toms friend Garfield the cat, adventure awaits in.... Adventures of Tom the cat.", "tomandjerry");
+AddGame("Rat City", "A very meme heavy MTAG, we recommend reading all the lore", "ratcity");
 
 // End of adding games - Update game list
 updateGameList();
